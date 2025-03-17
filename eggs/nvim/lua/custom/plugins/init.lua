85ef608d78e12eea90ec53fb79bd0b5db8aa8696
leo-n0 <hobby.leo@posteo.de>
2025-03-17 05:20:24 -0400

    niri colors + colorizer adjustments

--- eggs/nvim/lua/custom/plugins/init.lua
+++ eggs/nvim/lua/custom/plugins/init.lua
@@ -9,7 +9,11 @@ return {
     lazy = false,
     dependencies = { 'folke/tokyonight.nvim' },
     config = function()
-      require('colorizer').setup()
+      require('colorizer').setup({ '*' }, {
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+      })
     end,
   },
 }
